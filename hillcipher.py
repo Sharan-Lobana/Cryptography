ae7beec2b3a634909560176e1a13746604afbf03
Sharanpreet Singh <sps27iitr@gmail.com>
2016-01-07 21:24:14 +0530

    Corrected hillcipher.py

--- hillcipher.py
+++ hillcipher.py
@@ -1,9 +1,8 @@
 #Hill Cipher implementation in python
-#This implementation is not complete.
-# It needs some improvement
 
 #The integers 0-25 map to A-Z
-#The cipher and deciphered text is assumed to be in capitals
+#The message to be encrypted can contain characters in a-z,A-Z and whitespace
+#The ciphered text is assumed to be in uppercase alphabets
 
 import random
 import numpy as np 
@@ -25,6 +24,30 @@ def gcd(b,a):
 		return b
 	return gcd(a%b,b)
 
+#Utility function to find the modular multiplicative inverse
+def modular_multiplicative_inverse(a,n):
+	t = 0
+	nt = 1
+	r = n
+	nr = a%n 
+	if n < 0:
+		n = -n
+	if a < 0:
+		a = n - (-a%n)
+	while nr != 0:
+		quot = (r/nr) | 0
+		tmp = nt 
+		nt = t - quot*nt 
+		t = tmp
+		tmp = nr 
+		nr = r - quot*nr 
+		r = tmp
+	if r > 1:
+		return -1
+	if t < 0:
+		t += n
+	return t 
+
 #Function for generating the 3x3 key for encryption
 def generate_key():
 	key = [[0 for x in range(3)] for x in range(3)]
@@ -40,16 +63,15 @@ def generate_key():
 	if round(np.linalg.det(key_matrix)) == 0:
 		return generate_key()
 
-	#Generate a new key if the determinant has common factor with 26
-	#Instead of using inverse we will be using the adjoint of the key_matrix
-	#for modular operations since the inverse can be in floating point
-	#Inverse_matrix equals adjoint_matrix/scalar_determinant
-	#if gcd(det,26) == 0 then inverse_matrix%26 is same as adjoint_matrix%26
+	#Generate a key if modular inverse of det w.r.t (mod 26) doesn't exist
+	#i.e if the gcd of determinant and 16 is not equal to 1
 	elif gcd(round(np.linalg.det(key_matrix))%26,26) != 1:
 		return generate_key()
 
 	return key_matrix
 
+#The append_value specifies the character to be appended to 
+#text to make its length a multiple of 3 for complete trigrams 
 def encipher(message,key_matrix,append_value='X'):
 	message = message.replace(' ','')
 	message = message.upper()
@@ -62,7 +84,6 @@ def encipher(message,key_matrix,append_value='X'):
 
 	for x in range(remaining):
 		message = message + append_value
-	print message
 	num_trigrams = len(message)/3	
 	for i in range(num_trigrams):
 		tri = message[3*i:3*(i+1)]
@@ -84,25 +105,30 @@ def decipher(cipher,key_matrix):
 	#than A-Z or length of cipher is not a multiple of 3
 	if any(ord(x) > 90 or ord(x) < 65 for x in cipher) or length%3 != 0:
 		return "Illegal cipher"
-
+	d = round(np.linalg.det(key_matrix))
 	#If the key_matrix is non-invertible return 
-	if round(np.linalg.det(key_matrix)) == 0:
+	if  d == 0:
 		return "Invalid key" 
 	original_message = ''
 
 	#Compute the inverse of the key array
-	inverse = np.linalg.inv(key_matrix)
+	matrix_inverse = np.linalg.inv(key_matrix)
 
-	#Find the adjoint from the inverse
-	adjoint = np.rint(inverse*round(np.linalg.det(key_matrix))).astype(int)
+	#Compute the adjoint from the inverse
+	adjoint = np.rint(matrix_inverse*d).astype(int)
 
+	#Compute the modular multiplicative inverse of determinant
+	d_inverse  = modular_multiplicative_inverse(int(d),26)
+
+	#Compute the modular multiplicative inverse for the key_matrix
+	inverse = d_inverse*adjoint
 	for i in range(length/3):
 
 		#Select a trigram from the cipher
 		tri = cipher[3*i:3*(i+1)]
 		c = [char_to_int(x) for x in tri] #Convert the chars to integers in 0-25
 		c_vector = np.matrix(c) #declare a numpy vector
-		p_vector = (adjoint*c_vector.T).astype(int)%26 #Decrypt the cipher
+		p_vector = (inverse*c_vector.T).astype(int)%26 #Decrypt the cipher
 
 		#Perform integer to character mapping
 		for x in p_vector:
